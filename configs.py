b8b2e288e5dc7925da33f95772c96b8731c5484c
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-09-29 11:35:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ class Config(object):
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001868162542"))
 	SHORTLINK_URL = os.environ.get('SHORTLINK_URL', 'sharelinks.in')
 	SHORTLINK_API = os.environ.get('SHORTLINK_API', '0e5f2972d232c6d9dbe4bfbd06654367866d1c1e')
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5674333293"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1150044195"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://theamanchaudhary:updatesbyaman@cluster0.rlgbj7c.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001497086126")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001716327321")
